dac5f43748b7a4545c4a3254f78c641c1316c2e1
DavyCraft648 <davyaulianugraha@gmail.com>
2022-03-18 01:40:59 +0700

    Changed words, add #1

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # <img height="64" src="https://github.com/DavyCraft648/ItemLifeTime/blob/main/icon.png" width="64"/> ItemLifeTime
-Set custom dropped item lifetime in your server
+Adjust the time of dropped items to disappear in your server
 
-## How does this works?
-Default lifetime for dropped items is 5 minutes (300 seconds)
+## How does this plugin work?
+Default time of dropped items to disappear in PocketMine is 5 minutes (300 seconds),
 but you can change it with this plugin!
 
 ## How to use?
-- `item-lifetime` in config.yml is used to change the lifetime in your whole server.
+- `item-lifetime` in config.yml is used to change dropped items' time to disappear in your whole server.
 
 
 - Use `worlds` in the config if you want a separated item lifetime on each world in your server,
@@ -14,7 +14,7 @@ but you can change it with this plugin!
   ```yaml
   worlds:
     MyPrettyWorld: 120 # Set item lifetime on MyPrettyWorld world to 120 second
-    MyBadWorld: 360 # Set item lifetime on MyBadWorld world to 360 second
+    MyDirtyWorld: 360 # Set item lifetime on MyDirtyWorld world to 360 second
   ```
 
 ## Config
@@ -22,20 +22,28 @@ but you can change it with this plugin!
 --- # ItemLifeTime plugin configuration file
 
 # Set dropped item lifetime in your server (in second)
-# Default is 5 minutes (300 seconds)
+# Default is 300 seconds (5 minutes)
 # Minimum value is 0, maximum is 1938
 # Item will never despawn if this set to -1
 item-lifetime: 300
 
 # Set different custom item lifetime on each of these worlds (optional)
+# Value in item-lifetime will be used if the world is not set in this list
 # Example configuration:
 # worlds:
 #   MyPrettyWorld: 120
-#   MyBadWorld: 360
+#   MyDirtyWorld: 360
 #
-# This will set item lifetime in MyPrettyWorld world to 120 seconds, 360 seconds in MyBadWorld, else 300
-# seconds in the other worlds
+# This will set item lifetime in MyPrettyWorld world to 120 seconds, 360 seconds in MyDirtyWorld
 worlds: {}
+
+# Show remaining time to disappear in item's nametag
+display-time:
+  enabled: false
+  # Text to be set in item's nametag
+  # {MINUTE}, {SECOND}, {TIME}
+  # Set this to "{MINUTE}:{SECOND} {TIME}" will be shown as "01:20 01 minute" (example)
+  text: "{MINUTE}:{SECOND}"
 ...
 
 ```
--- src/DavyCraft648/ItemLifeTime/Main.php
+++ src/DavyCraft648/ItemLifeTime/Main.php
@@ -6,18 +6,27 @@ use pocketmine\entity\object\ItemEntity;
 use pocketmine\event\entity\ItemDespawnEvent;
 use pocketmine\event\entity\ItemSpawnEvent;
 use pocketmine\event\EventPriority;
+use pocketmine\scheduler\ClosureTask;
+use function explode;
 use function floor;
+use function gmdate;
+use function str_replace;
 
 final class Main extends \pocketmine\plugin\PluginBase {
 
-    private int $defaultDespawnDelay = 300; // default = 5 minutes
+    private int $defaultDespawnDelay = 60 * 5; // default = 5 minutes
     private array $customWorlds = [];
+    private bool $displayTime = false;
+    private string $displayText = "";
+    /** @var ItemEntity[] */
+    private array $itemToUpdate = [];
 
     protected function onEnable(): void {
         $this->saveDefaultConfig();
         $this->checkConfig();
 
-        $this->getServer()->getPluginManager()->registerEvent(ItemSpawnEvent::class, function(ItemSpawnEvent $event): void {
+        $plmanager = $this->getServer()->getPluginManager();
+        $plmanager->registerEvent(ItemSpawnEvent::class, function(ItemSpawnEvent $event): void {
             $entity = $event->getEntity();
             if (!($entity instanceof ItemEntity)) {
                 return;
@@ -29,8 +38,11 @@ final class Main extends \pocketmine\plugin\PluginBase {
             if ($age > $delay) {
                 $entity->setDespawnDelay($delay);
             }
+            if ($this->displayTime && $delay !== -1) {
+                $this->itemToUpdate[$entity->getId()] = $entity;
+            }
         }, EventPriority::LOW, $this);
-        $this->getServer()->getPluginManager()->registerEvent(ItemDespawnEvent::class, function(ItemDespawnEvent $event): void {
+        $plmanager->registerEvent(ItemDespawnEvent::class, function(ItemDespawnEvent $event): void {
             $entity = $event->getEntity();
             if (!($entity instanceof ItemEntity)) {
                 return;
@@ -41,12 +53,51 @@ final class Main extends \pocketmine\plugin\PluginBase {
                 $event->cancel();
             }
         }, EventPriority::LOW, $this);
+
+        if ($this->displayTime) {
+            $this->getScheduler()->scheduleDelayedRepeatingTask(new ClosureTask(function(): void {
+                foreach ($this->itemToUpdate as $id => $item) {
+                    if ($item->isClosed()) {
+                        unset($this->itemToUpdate[$id]);
+                        continue;
+                    }
+                    $despawnDelay = $item->getDespawnDelay() / 20;
+                    if ($despawnDelay < 0) {
+                        continue;
+                    }
+                    [$s, $m] = explode(":", gmdate("s:i", (int) $despawnDelay));
+                    try {
+                        $item->setNameTag(str_replace(
+                            ["{SECOND}", "{MINUTE}", "{TIME}"],
+                            [$s, $m, match (true) {
+                                $m > 1 => "$m minutes",
+                                $m > 0 => "$m minute",
+                                $s > 1 => "$s seconds",
+                                $s > 0 => "$s second"
+                            }],
+                            $this->displayText
+                        ));
+                    } catch (\UnhandledMatchError) {
+                    }
+                    $item->setNameTagVisible(true);
+                    $item->setNameTagAlwaysVisible(true);
+                }
+            }), 20, 10);
+        }
     }
 
     private function checkConfig(): void {
         $config = $this->getConfig();
+        $config->setDefaults([
+            "item-lifetime" => 300,
+            "worlds" => [],
+            "display-time" => [
+                "enabled" => false,
+                "text" => "{MINUTE}:{SECOND}"
+            ]
+        ]);
         $despawnDelay = $config->get("item-lifetime", 300);
-        if (!$this->checkDespawnDelay($despawnDelay)) {
+        if (!$this->isValidDespawnDelay($despawnDelay)) {
             $this->getLogger()->warning("\"item-lifetime\" must be in range 0 ... " . floor(ItemEntity::MAX_DESPAWN_DELAY / 20) . " or " . ItemEntity::NEVER_DESPAWN . ", got $despawnDelay");
             $this->getLogger()->info("Item lifetime was set to default value (300 seconds)");
         } else {
@@ -54,17 +105,31 @@ final class Main extends \pocketmine\plugin\PluginBase {
         }
 
         foreach ($config->get("worlds", []) as $name => $delay) {
-            if ($this->checkDespawnDelay($delay)) {
+            if ($this->isValidDespawnDelay($delay)) {
                 $this->customWorlds[$name] = $delay;
                 continue;
             }
 
             $this->getLogger()->info("Item lifetime in \"$name\" world was set to default value ($this->defaultDespawnDelay seconds)");
         }
+
+        $this->displayTime = $config->getNested("display-time.enabled", false);
+        if (empty($text = $this->displayTime ? $config->getNested("display-time.text", "") : "")) {
+            $this->displayTime = false;
+        }
+        $this->displayText = $text;
+        if ($config->hasChanged()) {
+            $config->save();
+        }
     }
 
-    public function checkDespawnDelay(int $despawnDelay): bool {
-        return !((($despawnDelay * 20) < 0 or ($despawnDelay * 20) > ItemEntity::MAX_DESPAWN_DELAY) and $despawnDelay !== ItemEntity::NEVER_DESPAWN);
+    /**
+     * Check for invalid despawn delay
+     * @param int $despawnDelay Despawn delay in second
+     * @return bool
+     */
+    public function isValidDespawnDelay(int $despawnDelay): bool {
+        return !((($despawnDelay * 20) < 0 || ($despawnDelay * 20) > ItemEntity::MAX_DESPAWN_DELAY) && $despawnDelay !== ItemEntity::NEVER_DESPAWN);
     }
 
 }
